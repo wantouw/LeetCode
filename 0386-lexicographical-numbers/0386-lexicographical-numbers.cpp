f120678603b8cf45180674e7a289ef51bd995e20
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-09-21 10:29:21 +0700

    Time: 15 ms (57.04%), Space: 14.1 MB (51.9%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    void tree(string curr, int max, vector<int>& result){
-        int value = stoi(curr);
-        if(value > max || value == 0) return;
-        cout << value << endl;
-        if(value!=0) result.push_back(value);
-        tree(curr + "0", max, result);
-        tree(curr + "1", max, result);
-        tree(curr + '2', max, result);
-        tree(curr + '3', max, result);
-        tree(curr + '4', max, result);
-        tree(curr + '5', max, result);
-        tree(curr + '6', max, result);
-        tree(curr + '7', max, result);
-        tree(curr + '8', max, result);
-        tree(curr + '9', max, result);
+    void tree(int curr, int max, vector<int>& result){
+        // int value = stoi(curr);
+        if(curr > max || curr == 0) return;
+        // cout << value << endl;
+        if(curr!=0) result.push_back(curr);
+        tree(curr * 10, max, result);
+        tree(curr * 10 + 1, max, result);
+        tree(curr * 10 + 2, max, result);
+        tree(curr * 10 + 3, max, result);
+        tree(curr * 10 + 4, max, result);
+        tree(curr * 10 + 5, max, result);
+        tree(curr * 10 + 6, max, result);
+        tree(curr * 10 + 7, max, result);
+        tree(curr * 10 + 8, max, result);
+        tree(curr * 10 + 9, max, result);
     }
     vector<int> lexicalOrder(int n) {
         vector<int> result;
-        tree("1", n, result);
-        tree("2", n, result);
-        tree("3", n, result);
-        tree("4", n, result);
-        tree("5", n, result);
-        tree("6", n, result);
-        tree("7", n, result);
-        tree("8", n, result);
-        tree("9", n, result);
+        tree(1, n, result);
+        tree(2, n, result);
+        tree(3, n, result);
+        tree(4, n, result);
+        tree(5, n, result);
+        tree(6, n, result);
+        tree(7, n, result);
+        tree(8, n, result);
+        tree(9, n, result);
         return result;
     }
 };
