5f5b18347a408c89d27e1c4cd7cc50f188749697
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-09-22 22:33:33 +0700

    Time: 0 ms (100%), Space: 7.4 MB (84.46%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,33 +1,5 @@
 class Solution {
 public:
-    // int test(int value, int max, int &index, int target){
-    //     int result;
-    //     if(value > max) return -1;
-    //     if(value != 0) index++;
-    //     if(index == target) return value;
-    //     if(value != 0) result = test(value * 10, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 1, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 2, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 3, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 4, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 5, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 6, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 7, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 8, max, index, target);
-    //     if(result != -1) return result;
-    //     result = test(value * 10 + 9, max, index, target);
-    //     if(result != -1) return result;
-    //     return -1;
-    // }
-    // int 
     int findKthNumber(int n, int k) {
         int curr = 1;
         k--;
