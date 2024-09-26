04f203461bc21dde3015cf3783d32fec73491290
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-09-26 10:08:24 +0700

    Time: 81 ms (55.69%), Space: 42.8 MB (59.1%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,42 +1,23 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/detail/standard_policies.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    ordered_set;
-
 class MyCalendar {
-public:
-    ordered_set dateStart;
-    ordered_set dateEnd;
-    MyCalendar() {
-        // dates.insert(1);
-        // dates.insert(1);
-        // cout << (dates.order_of_key(1));
-    }
+private:
+    set<pair<int, int>> calendar;
 
+public:
     bool book(int start, int end) {
-        int smallerStart = dateStart.order_of_key(start);
-        int smallerEnd = dateEnd.order_of_key(end);
-        if ((smallerStart == smallerEnd) &&
-            (dateEnd.find_by_order(smallerStart - 1) == dateEnd.end() ||
-             start >= *(dateEnd.find_by_order(smallerStart - 1))) &&
-            (dateStart.find_by_order(smallerEnd) == dateStart.end() ||
-             end <= *(dateStart.find_by_order(smallerEnd)))) {
-            // cout << start << ' ' << *(dateEnd.find_by_order(smallerStart - 1))
-                //  << endl;
-            dateStart.insert(start);
-            dateEnd.insert(end);
-            return true;
+        const pair<int, int> event{start, end};
+        const auto nextEvent = calendar.lower_bound(event);
+        if (nextEvent != calendar.end() && nextEvent->first < end) {
+            return false;
         }
-        return false;
+
+        if (nextEvent != calendar.begin()) {
+            const auto prevEvent = prev(nextEvent);
+            if (prevEvent->second > start) {
+                return false;
+            }
+        }
+
+        calendar.insert(event);
+        return true;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
