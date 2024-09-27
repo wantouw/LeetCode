3b61f7f7e0da5443adfbf8397f6f95efccaa0013
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-09-27 08:22:22 +0700

    Time: 58 ms (98.54%), Space: 37.8 MB (96.56%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,26 +1,38 @@
 class MyCalendarTwo {
 public:
-    map<int, int> flags;
+    // map<int, int> flags;
+    vector<pair<int, int>> bookings;
+    vector<pair<int, int>> overlapped;
+
     MyCalendarTwo() {
         
     }
+
+    bool isOverlapped(int newStart, int newEnd, int start, int end){
+        if (max(start, newStart) < min(end, newEnd)){
+            return true;
+        }
+        return false;
+    }
     
     bool book(int start, int end) {
-        flags[start]++;
-        flags[end]--;
-        int tracker = 0;
-        for(auto itr = flags.begin();itr!=flags.end();itr++){
-            // cout << itr->first << ' ' << itr->second << endl;
-            tracker+=itr->second;
-            if(tracker >= 3){
-                flags[start]--;
-                flags[end]++;
+        for(pair<int, int> booking: overlapped){
+            if(isOverlapped(booking.first, booking.second, start, end)){
                 return false;
             }
         }
+
+        for(pair<int, int> booking: bookings){
+            if(isOverlapped(booking.first, booking.second, start, end)){
+                overlapped.push_back(make_pair(max(start, booking.first), min(end, booking.second)));
+            }
+        }
+        bookings.push_back(make_pair(start, end));
+
         return true;
-        
     }
+
+
 };
 
 /**
