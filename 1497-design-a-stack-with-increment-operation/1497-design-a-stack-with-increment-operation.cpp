d9472ae9e46f2b26b95425f020d0b492ea52e27a
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-09-30 09:16:30 +0700

    Time: 20 ms (93.14%), Space: 25.9 MB (66.84%) - LeetHub

--- 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
@@ -1,77 +1,34 @@
-class Node {
-    public:
-    int x;
-    Node* prev;
-    Node* next;
-    Node(int x){
-        this->x = x;
-        this->prev = nullptr;
-        this->next = nullptr;
-    }
-};
-
 class CustomStack {
+private:
+    // Vector to store stack elements
+    vector<int> stackVector;
+
+    // Index of the top element in the stack
+    int topIndex;
+
 public:
-    Node* head;
-    Node* tail;
-    int size;
-    int max;
     CustomStack(int maxSize) {
-        head = nullptr;
-        tail = nullptr;
-        max = maxSize;
-        size = 0;
+        stackVector.resize(maxSize);
+        topIndex = -1;
     }
-    
+
     void push(int x) {
-        Node* newNode = new Node(x);
-        if(size == max) return;
-        if(!head){
-            head = newNode;
-            tail = newNode;
+        if (topIndex < (int)(stackVector.size()) - 1) {
+            stackVector[++topIndex] = x;
         }
-        else{
-            // cout << head->x << endl;
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
-        size++;
     }
-    
+
     int pop() {
-        if(!head) return -1;
-        int result = -1;
-        if(head == tail){
-            result = head->x;
-            delete head;
-            head = tail = nullptr;
+        if (topIndex >= 0) {
+            return stackVector[topIndex--];
         }
-        else{
-            result = tail->x;
-            tail = tail->prev;
-            // Node* temp = tail->next;
-            delete tail->next;
-            tail->next = nullptr;
-        }
-        size--;
-        return result;
+        return -1;  // Return -1 if the stack is empty
     }
-    
+
     void increment(int k, int val) {
-        Node* curr = head;
-        for(int i = 0;i<k;i++){
-            if(!curr) return;
-            curr->x+=val;
-            curr = curr->next;
+        int limit = min(k, topIndex + 1);
+        for (int i = 0; i < limit; i++) {
+            stackVector[i] += val;
         }
     }
 };
-
-/**
- * Your CustomStack object will be instantiated and called as such:
- * CustomStack* obj = new CustomStack(maxSize);
- * obj->push(x);
- * int param_2 = obj->pop();
- * obj->increment(k,val);
- */
