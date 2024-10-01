72747053e696d922311d806c5785bf75ee795e31
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-10-01 08:20:59 +0700

    Time: 156 ms (21.16%), Space: 79.7 MB (14.02%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -4,29 +4,18 @@ public:
     bool canArrange(vector<int>& arr, int k) {
         int cannots = 0;
         for (int i : arr) {
-            int remainder = i % k;
-            // cout << "i: " << i << "rem: " << remainder << endl;
+            int remainder = (i % k + k) % k;
             if (map[k - remainder] > 0) {
-            // cout << i << " rem " << remainder << ' ' << k-remainder << endl;
                 map[k - remainder]--;
-                // map[remainder]--;
                 cannots--;
             } else if (map[0 - remainder] > 0) {
-            // cout << i << " rem " << remainder << ' ' << 0-remainder << endl;
                 map[0 - remainder]--;
-                // map[remainder]--;
-                cannots--;
-            } else if (map[-k - remainder] > 0) {
-            // cout << i << " rem " << remainder << ' ' << 0-remainder << endl;
-                map[-k - remainder]--;
-                // map[remainder]--;
                 cannots--;
             }else {
                 cannots++;
                 map[remainder]++;
             }
         }
-        // cout << "cannots " << cannots << endl;
         if (cannots > 0)
             return false;
         return true;
