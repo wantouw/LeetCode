71369438ef5dbc02a3a5ecde9b55833c57b5217c
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-10-04 08:13:32 +0700

    Time: 51 ms (95.6%), Space: 56.5 MB (56.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -2,20 +2,11 @@ class Solution {
 public:
     
     long long dividePlayers(vector<int>& skill) {
-        // int target = 0, sum = 0;
         sort(skill.begin(), skill.end());
-        // for(int value: skill){
-        //     map[value]++;
-        //     sum+= value;
-        // }
-        // target = sum / (skill.size() / 2);
-
-        // cout << skill << endl;
         long long int result = 0;
-        int target = skill[0] + skill[skill.size() - 1];
         int length = skill.size();
+        int target = skill[0] + skill[length - 1];
         for(int i = 0; i < length / 2 ; i++){
-            // cout << skill[i] << endl;
             if(skill[i] + skill[length - 1 - i] == target){
                 result += skill[i] * skill[length - i - 1];
             }
