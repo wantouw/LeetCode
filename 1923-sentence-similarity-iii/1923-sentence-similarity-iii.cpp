edeba8a332cb9b7475e40e4222e0268d91a9b2be
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-10-06 14:51:32 +0700

    Time: 0 ms (100%), Space: 7.9 MB (90.78%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -1,38 +1,6 @@
 class Solution {
 public:
-    // string firstWord(string word){
-    //     string result;
-    //     int length = word.length();
-    //     for(int i = 0; i < length ; i++){
-    //         if(word[i] == ' ' ){
-    //             break;
-    //         }
-    //         result+= word[i];
-    //     }
-    //     return result;
-    // }
-    // bool isOneWord(string word){
-    //     for(char letter: word){
-    //         if(letter == ' '){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-    // string lastWord(string word){
-    //     string result;
-    //     int length = word.length();
-    //     for(int i = length - 1; i >=0 ; i--){
-    //         if(word[i] == ' '){
-    //             break;
-    //         }
-    //         result.insert(result.begin(), word[i]);
-    //     }
-    //     // reverse(result.begin(), result.end());
-    //     return result;
-    // }
     int frontSimilarity(string sentence1, string sentence2, int minLen) {
-        // int minLen = min(sentence1.length(), sentence2.length());
         int similarity = 0;
         int total = 0;
         for (int i = 0; i < minLen; i++) {
@@ -72,15 +40,9 @@ public:
         return total;
     }
     bool areSentencesSimilar(string sentence1, string sentence2) {
-        // string first1 = firstWord(sentence1), first2 = firstWord(sentence2);
-        // string last1 = lastWord(sentence1), last2 = lastWord(sentence2);
-        // bool isOne1 = isOneWord(sentence1), isOne2 = isOneWord(sentence2);
-        // cout << first1 << ' ' << last1 << ' ' << isOne1 << endl;
-        // cout << first2 << ' ' << last2 << ' ' << isOne2 << endl;
         int minLen = min(sentence1.length(), sentence2.length());
         int frontValue = frontSimilarity(sentence1, sentence2, minLen);
         int backValue = backSimilarity(sentence1, sentence2, minLen);
-        // cout << "front: " << frontValue << "back: " << backValue << endl;
         if(sentence1 == sentence2) return true;
         if (frontValue + backValue >= minLen) {
             return true;
