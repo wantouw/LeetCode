965d1ecb476a70954ec710f6d0a760a9365a40c9
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2024-10-07 22:26:05 +0700

    Time: 9 ms (24.64%), Space: 11.6 MB (31.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
-    int nextViable(string s, int start, int length){
-        for(int i = start ; i < length ; i++){
-            if(s[i]!='x'){
-                return i;
-                
-            }
-        }
-        return -1;
-    }
-    int prevViable(string s, int start, int length){
-        for(int i = start ; i >= 0 ; i--){
-            if(s[i]!='x'){
-                return i;
-                
-            }
-        }
-        return 0;
-    }
     int minLength(string s) {
-        int length = s.length();
-        int result = length;
-        for(int i = 0 ; i < length - 1 ; i ++){
-            int next =  nextViable(s, i+1, length);
-            // cout << "first next: " << s[i] << next << endl;
-            if(next == -1) continue;
-            if(s[i] == 'A' && s[next] == 'B'){
-                s[i] = 'x';
-                s[next] = 'x';
-                result-=2;
-                i = prevViable(s, i-1, length) - 1;
+        stack<char> stack;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s[i];
+
+            if (stack.empty()) {
+                stack.push(currentChar);
+                continue;
             }
-            next =  nextViable(s, i+1, length);
-            if(next == -1 || i <= -1) continue;
-            if(s[i] == 'C' && s[next] == 'D'){
-                s[i] = 'x';
-                s[next] = 'x';
-                result-=2;
-                i = prevViable(s, i-1, length) - 1;
+
+            if (currentChar == 'B' && stack.top() == 'A') {
+                stack.pop();
             }
-            // cout << s << endl;
+            else if (currentChar == 'D' && stack.top() == 'C') {
+                stack.pop();
+            }
+            else {
+                stack.push(currentChar);
+            }
         }
-        return result;
+
+        return stack.size();
     }
 };
