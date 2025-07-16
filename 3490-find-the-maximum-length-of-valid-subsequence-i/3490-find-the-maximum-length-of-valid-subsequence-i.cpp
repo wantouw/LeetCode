1cfd24a2184d5c5f1d2f3813d50141726b66f894
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-07-16 15:55:28 +0700

    Time: 4 ms (65.29%), Space: 96 MB (97.65%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        // int continuousMax = 0;
-        // int zigzagMax = 0;
-        // int continuous = 0;
-        // int zigzag = 0;
-        // nums[0] = nums[0] % 2;
-        // continuous = 1;
-        // zigzag = 1;
-        // for(int i = 1 ; i < nums.size() ; i++){
-        //     if(nums[i] % 2 == nums[i-1]){
-        //         continuous++;
-        //         zigzag = 1;
-        //         if(continuous > continuousMax) continuousMax = continuous;
-        //     }
-        //     else {
-        //         zigzag++;
-        //         continuous = 1;
-        //         if(zigzag > zigzagMax) zigzagMax = zigzag;
-        //     }
-        //     nums[i] = nums[i]%2;
-        // }
-        // vector<int> zigzag;
-        // vector<int> continuous;
         nums[0] = nums[0]%2;
         int zigzag = nums[0];
         int continuous = nums[0];
