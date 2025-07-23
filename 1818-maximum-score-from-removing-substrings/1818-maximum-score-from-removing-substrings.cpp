0d2b099e765c5c3198454b5f39af099e8b45fd6a
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-07-23 11:16:25 +0700

    Time: 14 ms (96.15%), Space: 19.7 MB (99.36%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,61 +1,38 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        // stack<char> removed;
         int points = 0;
-        int size = s.size();
+        char first = 'b';
+        char second = 'a';
+        int count1 = 0, count2 = 0;
+        int high = y;
+        int low = x;
         if(x > y){
-            for(int i = 1 ; i < size ; i++){
-                if(s[i] == 'b' && i - 1 >= 0 && s[i-1] == 'a'){
-                    // cout << "kena" << endl;
-                    points+=x;
-                    // removed = removed.substr(0, removedSize-1);
-                    // s = s.substr(0, i-1) + s.substr(i+1);
-                    s.erase(i-1, 2);
-                    size-=2;
-                    i-=2;
-                    // removedSize--;
-                }
-                // cout << "hai " << removed << endl;
+            first = 'a';
+            second = 'b';
+            high = x;
+            low = y;
+        }
+        for(char c: s){
+            if(c == first){
+                count1++;
             }
-            // cout << "size: " << removedSize << endl;
-            for(int i = 1 ; i < size ; i++){
-                if(s[i] == 'a' && i - 1 >= 0 && s[i-1] == 'b'){
-                // cout << removed[i] << " " << removed[i-1] << endl;
-                    // cout << "kena" << endl;
-                    points+=y;
-                    s.erase(i-1, 2);
-                    size-=2;
-                    i-=2;
+            else if(c == second){
+                if(count1 > 0){
+                    count1--;
+                    points+=high;
+                } else {
+                    count2++;
                 }
-                // cout << "second " << removed << endl;
+            }
+            else{
+                points+= min(count2, count1) * low;
+                count1=0;
+                count2=0;
             }
         }
-        else {
-            for(int i = 1 ; i < size ; i++){
-                if(s[i] == 'a' && i - 1 >= 0 && s[i-1] == 'b'){
-                    // cout << "kena" << endl;
-                    points+=y;
-                    // removed = removed.substr(0, removedSize-1);
-                    s.erase(i-1, 2);
-                    size-=2;
-                    i-=2;
-                    // removedSize--;
-                }
-                // cout << "hai " << removed << endl;
-            }
-            // cout << "size: " << removedSize << endl;
-            for(int i = 1 ; i < size ; i++){
-                // cout << removed[i] << " " << removed[i-1] << endl;
-                if(s[i] == 'b' && i - 1 >= 0 && s[i-1] == 'a'){
-                    // cout << "kena" << endl;
-                    points+=x;
-                    s.erase(i-1, 2);
-                    size-=2;
-                    i-=2;
-                }
-                // cout << "second " << removed << endl;
-            }
+        if(count1 > 0){
+            points+= min(count2, count1) * low;
         }
         return points;
     }
