5a1f33b1fae03dae22dca21dbb68469155ff34ab
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-07-25 09:42:25 +0700

    Time: 0 ms (100%), Space: 29.7 MB (37.84%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        unordered_map<int, int> map;
+        unordered_set<int> set;
         int max = 0;
         int largest = INT_MIN;
         for(auto num: nums){
             if(num > 0){
-                map[num]++;
+                if(set.count(num) == 0) max+=num;
+                set.insert(num);
             }
-            if(map[num] == 1) max+=num;
             if(num > largest) largest = num;
         }
         if(largest < 0) return largest;
