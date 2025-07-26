3c39cf99a43129533cbe2c2eba8dd5cd3b380de5
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-07-26 10:46:33 +0700

    Time: 503 ms (22.03%), Space: 420.1 MB (5.09%) - LeetHub

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.cpp
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -4,24 +4,46 @@ public:
         pair<int, int> leftLimit;
         leftLimit.first = 0;
         leftLimit.second = 0;
-        unordered_map<int, vector<int>> rightLimit;
+        unordered_map<int, pair<int, int>> rightLimit;
         // int size = n.size();
         long long int maxSurplus = 0;
-        for(auto pair: conflictingPairs){
-            rightLimit[max(pair[0], pair[1])].push_back(min(pair[0], pair[1]));
+        for(auto pairIdx: conflictingPairs){
+            int high = max(pairIdx[0], pairIdx[1]);
+            int low = min(pairIdx[0], pairIdx[1]);
+            pair<int, int>& currPair = rightLimit[high];
+            if(low > currPair.first){
+                currPair.second = currPair.first;
+                currPair.first = low;
+            }
+            else if(low > currPair.second){
+                currPair.second = low;
+            }
+            // rightLimit[max(pairIdx[0], pairIdx[1])].push_back(min(pairIdx[0], pairIdx[1]));
         }
         vector<long long int> surplus(n);
         long long int ans = 0;
         for(int i = 1; i <= n ; i++){
-            for(auto left: rightLimit[i]){
-                if(left > leftLimit.first){
-                    leftLimit.second = leftLimit.first;
-                    leftLimit.first = left;
-                }
-                else if(left > leftLimit.second){
-                    leftLimit.second = left;
+            // for(auto left: rightLimit[i]){
+            //     if(left > leftLimit.first){
+            //         leftLimit.second = leftLimit.first;
+            //         leftLimit.first = left;
+            //     }
+            //     else if(left > leftLimit.second){
+            //         leftLimit.second = left;
+            //     }
+            // }
+            pair<int, int> currPair = rightLimit[i];
+            if(currPair.first > leftLimit.first){
+                leftLimit.second = leftLimit.first;
+                leftLimit.first = currPair.first;
+                if(currPair.second > leftLimit.second){
+                    leftLimit.second = currPair.second;
                 }
             }
+            else if(currPair.first > leftLimit.second){
+                leftLimit.second = currPair.first;
+            }
+
             ans += i - leftLimit.first;
             // cout << i << " " << leftLimit.first << " " << leftLimit.second << endl;
             surplus[leftLimit.first] += leftLimit.first - leftLimit.second;
