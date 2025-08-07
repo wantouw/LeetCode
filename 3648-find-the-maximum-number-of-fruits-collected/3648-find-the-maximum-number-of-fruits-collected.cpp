9541c588d95b59c9fd3e2f12a0ca9510e4d57945
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-08-07 21:58:39 +0700

    Time: 14 ms (97.4%), Space: 137.1 MB (94.16%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
@@ -3,12 +3,10 @@ public:
     int maxCollectedFruits(vector<vector<int>>& fruits) {
         int size = fruits.size();
         int firstMax = 0;
-        int maxFruit = 0;
         for(int i = 0 ; i < size ; i++){
             firstMax+=fruits[i][i];
             fruits[i][i] = 0;
         }
-        int secondMax = fruits[0][size-1];
         for(int i = 1 ; i <= size / 2 ; i++){
             if(i == size / 2 && size % 2 == 0) break;
             if(i == 1){
@@ -21,15 +19,10 @@ public:
                 int right = size - j;
                 int mid = size - j - 1;
                 int left = size - j - 2;
-                // cout << "curr: " << fruits[i][size - j - 1] << endl;
                 if(right < size) maxValue = max(maxValue, fruits[i-1][right]);
-                // if(right < size) cout << "right: " << fruits[i-1][right];
                 if(mid > size - i - 1) maxValue = max(maxValue, fruits[i-1][mid]);
-                // if(mid > size - i - 1) cout << " mid: " << fruits[i-1][mid];
                 if(left > size - i - 1) maxValue = max(maxValue, fruits[i-1][left]);
-                // if(left > size - i - 1) cout << " left: " << fruits[i-1][left];
                 fruits[i][size - j - 1] += maxValue; 
-                // cout << endl;
             }
         }
         int midSize = size / 2 - 1;
@@ -40,23 +33,16 @@ public:
                 int right = size - j;
                 int mid = size - j - 1;
                 int left = size - j - 2;
-                // cout << "curr: " << fruits[midSize + i][size - j - 1] << endl;
                 if(right < size) maxValue = max(maxValue, fruits[midSize + i-1][right]);
-                // if(right < size) cout << "right: " << fruits[midSize + i-1][right];
                 maxValue = max(maxValue, fruits[midSize + i-1][mid]);
-                // cout << " mid: " << fruits[midSize + i-1][mid];
                 maxValue = max(maxValue, fruits[midSize + i-1][left]);
-                // cout << " left: " << fruits[midSize + i-1][left];
                 fruits[midSize + i][size - j - 1] += maxValue; 
-                // cout << endl;
             }
         }
-        secondMax = max(fruits[size-2][size-2], fruits[size-2][size-1]);
-        // cout << "secondMax: " << secondMax << endl;
+        int secondMax = max(fruits[size-2][size-2], fruits[size-2][size-1]);
         for(int i = 0 ; i < size ; i++){
             fruits[i][i] = 0;
         }
-        int thirdMax = fruits[size-1][0];
         for(int i = 1 ; i <= size / 2 ; i++){
             if(i == size / 2 && size % 2 == 0) break;
             if(i == 1){
@@ -69,15 +55,10 @@ public:
                 int right = size - j;
                 int mid = size - j - 1;
                 int left = size - j - 2;
-                // cout << "curr: " << fruits[i][size - j - 1] << endl;
                 if(right < size) maxValue = max(maxValue, fruits[right][i-1]);
-                // if(right < size) cout << "right: " << fruits[right][i-1];
                 if(mid > size - i - 1) maxValue = max(maxValue, fruits[mid][i-1]);
-                // if(mid > size - i - 1) cout << " mid: " << fruits[mid][i-1];
                 if(left > size - i - 1) maxValue = max(maxValue, fruits[left][i-1]);
-                // if(left > size - i - 1) cout << " left: " << fruits[left][i-1];
                 fruits[size-j-1][i] += maxValue; 
-                // cout << endl;
             }
         }
         for(int i = 1 ; i < size / 2 ; i++){
@@ -86,19 +67,13 @@ public:
                 int right = size - j;
                 int mid = size - j - 1;
                 int left = size - j - 2;
-                // cout << "curr: " << fruits[size - j - 1][midSize + i] << endl;
                 if(right < size) maxValue = max(maxValue, fruits[right][midSize + i-1]);
-                // if(right < size) cout << "right: " << fruits[right][midSize + i-1];
                 maxValue = max(maxValue, fruits[mid][midSize + i-1]);
-                // cout << " mid: " << fruits[mid][midSize + i-1];
                 maxValue = max(maxValue, fruits[left][midSize + i-1]);
-                // cout << " left: " << fruits[left][midSize + i-1];
                 fruits[mid][midSize + i] += maxValue; 
-                // cout << endl;
             }
         }
-        thirdMax = max(fruits[size-2][size-2], fruits[size-1][size-2]);
-        // cout << firstMax << " " << secondMax << " " << thirdMax << endl;
+        int thirdMax = max(fruits[size-2][size-2], fruits[size-1][size-2]);
         return firstMax + secondMax + thirdMax;
     }
 };
