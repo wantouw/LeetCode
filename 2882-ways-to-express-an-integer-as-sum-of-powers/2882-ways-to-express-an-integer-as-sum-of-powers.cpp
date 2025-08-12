a7092287dd874034f8451b87884a5a0573e2bf8c
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-08-12 22:21:06 +0700

    Time: 163 ms (58.39%), Space: 9.4 MB (99.78%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
-    long long int dp[301][301] = {};
-    long long power(int base, int exp) {
-        long long res = 1;
+    int dp[301][301] = {};
+    int power(int base, int exp) {
+        int res = 1;
         for (int i = 0; i < exp; ++i) {
             res *= base;
             if (res > 300) return 301; 
         }
         return res;
     }
-    // long long int calc(int curr, long long int& result, int x, int remaining){
-    //     if(dp[curr][remaining] > 0){
-    //         cout << "curr: " << curr << " remaining: " << remaining << " 1" << endl;
-    //         result++;
-    //         return dp[curr][remaining];
-    //     }
-    //     if(dp[curr][remaining] == -1) return -1;
-    //     long long int currResult = power(curr, x);
-    //     if(currResult == remaining){
-    //         result++;
-    //         cout << "curr: " << curr << " remaining: " << remaining << " 2" << endl;
-    //         dp[curr][remaining] = 1;
-    //         return 1;
-    //     }
-    //     if(currResult > remaining){
-    //         dp[curr][remaining] = -1;
-    //         return -1;
-    //     }
-    //     long long int pos = 0;
-    //     for(int i = curr+1 ; i <= remaining ; i++){
-    //         pos+=max((long long int)0, calc(i, result, x, remaining - currResult));
-    //     }
-    //     return dp[curr][remaining] = pos;
-    // }
     int calc(int curr, int x, int remaining){
         int result = power(curr, x);
         if(remaining == 0) return 1;
@@ -44,10 +20,6 @@ public:
         return dp[curr][remaining] = (include + exclude) % 1000000007;
     }
     int numberOfWays(int n, int x) {
-        long long int result = 0;
-        // for(int i = 1 ; i <= n ; i++){
-        //     calc(i, result, x, n);
-        // }
         return calc(1, x, n);
     }
 };
