457f54979f66be4d34f35c6c376a190d1f95696b
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-08-30 11:20:01 +0700

    Time: 0 ms (100%), Space: 21.7 MB (83.4%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,41 +2,23 @@ class Solution {
 public:
     int charToNum(char c){
         if(c >= '1' && c <= '9'){
-            return c - '0';
+            return c - '1';
         }
         return 0;
     }
     bool isValidSudoku(vector<vector<char>>& board) {
-        // cout << charToNum('1');
-        int SIZE = 9;
-        unordered_set<int> vertical;
-        unordered_set<int> horizontal;
-        for(int i = 0 ; i < SIZE ; i++){
-            for(int j = 0 ; j < SIZE ; j++){
-                int horizontalNum = charToNum(board[i][j]);
-                int verticalNum = charToNum(board[j][i]);
-                if(vertical.find(verticalNum)!= vertical.end() || 
-                    horizontal.find(horizontalNum) != horizontal.end()){
-                    return false;
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = charToNum(board[i][j]);
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
                 }
-                if(horizontalNum != 0) horizontal.insert(horizontalNum);
-                if(verticalNum != 0) vertical.insert(verticalNum);
-            }
-            horizontal.clear();
-            vertical.clear();
-        }
-        for(int i = 0 ; i < SIZE ; i+=3){
-            for(int j = 0 ; j < SIZE ; j+=3){
-                for(int k = 0 ; k < 3 ; k++){
-                    for(int l = 0 ; l < 3 ; l++){
-                        int number = charToNum(board[i+k][j+l]);
-                        if(horizontal.find(number) != horizontal.end()){
-                            return false;
-                        }
-                        if(number != 0) horizontal.insert(number);
-                    }
-                }
-                horizontal.clear();
             }
         }
         return true;
