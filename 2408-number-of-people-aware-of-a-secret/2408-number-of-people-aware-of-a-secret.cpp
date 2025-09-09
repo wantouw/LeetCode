2a6a8458db66b4ca1bedff1ad7182069faf79b3a
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-09-09 21:21:40 +0700

    Time: 4 ms (46.04%), Space: 9.6 MB (39.85%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -6,15 +6,12 @@ public:
         long long result = 0;
         int start = -1, end = -1;
         for(int i = 0 ; i < n ; i++){
-            // cout << dp[i] << endl;
             result += dp[i];
             if(dp[i] <= 0) continue;
             start = i + delay;
             end = i + forget;
-            // cout << "start: " << start << ", end: " << end << endl;
             for(int j = start ; j < end && j < n ; j++){
                 dp[j] += dp[i] % 1000000007;
-                // dp[j] %= 1000000007;
             }
             if(end < n) result -= dp[i];
             result %= 1000000007;
