6a22e9f470946d43bf5ab50db990eed2525f1ac3
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-09-10 15:41:55 +0700

    Time: 12 ms (95.29%), Space: 65.6 MB (85.88%) - LeetHub

---
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+        vector<bitset<501>> learned(languages.size()+5);
+        for (int i = 0; i < languages.size(); i++)
+        {
+            for (int j = 0; j < languages[i].size(); j++)
+            {
+                learned[i][languages[i][j]] = 1;
+            }
+        }
+        bitset<501> need;
+        for (auto friendship : friendships)
+        {
+            if (!((learned[friendship[0] - 1] & learned[friendship[1] - 1]).any()))
+            {
+                need[friendship[0] - 1] = 1;
+                need[friendship[1] - 1] = 1;
+            }
+        }
+        int result = 1000000;
+        for (int i = 1; i <= n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < languages.size(); j++)
+            {
+                // cout << j << " " << need[j] << " " << learned[j][i] << endl;
+                if (need[j] && !learned[j][i])
+                    count++;
+            }
+            // if (count > 0)
+                result = min(result, count);
+        }
+        return result;
+    }
+};
