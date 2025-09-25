e38c0dbe662487f63f4cd5b3368ddddbf1116298
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-09-25 17:40:36 +0700

    Time: 99 ms (4.44%), Space: 12.7 MB (81.37%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int height = triangle.size();
+        if(height == 1) return triangle[0][0];
+        int result = INT_MAX;
+        int curr = 0;
+        for(int i = 1 ; i < height ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                curr = triangle[i][j];
+                if(j == i){
+                    triangle[i][j] = curr + triangle[i-1][j-1];
+                }
+                else if(j > 0){
+                    cout << i << " " << j << endl;
+                    triangle[i][j] = min(curr + triangle[i-1][j], curr + triangle[i-1][j-1]);
+                }
+                else {
+                    triangle[i][j] = curr + triangle[i-1][j];
+                }
+                if(i == height - 1){
+                    result = min(result, triangle[i][j]);
+                }
+            }
+        }
+        return result;
+    }
+};
