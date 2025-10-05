af8092e22c5597f1b08c98025fe025683d3eb03b
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-10-05 16:43:06 +0700

    Time: 5 ms (72.28%), Space: 23.9 MB (41.27%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+
+    vector<vector<int>> atlantic;
+    vector<vector<int>> pacific;
+    int length, width;
+
+    int dfs(int i, int j, vector<vector<int>>& heights, vector<vector<int>>& sea){
+        if(i < 0 || j < 0 || i >= length || j >= width){
+            return 0;
+        }
+        if(sea[i][j]) return sea[i][j];
+        sea[i][j] = 1;
+        if(i > 0 && heights[i-1][j] >= heights[i][j]){
+            dfs(i-1, j, heights, sea);
+        }
+        if(j > 0 && heights[i][j-1] >= heights[i][j]){
+            dfs(i, j-1, heights, sea);
+            // cout << endl <<  "i: " << i << ' ' << j << ' ' << neighbor << endl;
+            // if(neighbor != result) result += neighbor;
+            // atlantic[i][j] = result;
+        }
+        if(j < width - 1 && heights[i][j+1] >= heights[i][j]){
+            dfs(i, j+1, heights, sea);
+            // if(neighbor != result) result += neighbor;
+            // atlantic[i][j] = result;
+        }
+        if(i < length - 1 && heights[i+1][j] >= heights[i][j]){
+            dfs(i+1, j, heights, sea);
+            // if(neighbor != result) result += neighbor;
+            // atlantic[i][j] = result;
+        }
+        return 0;
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        length = heights.size();
+        width = heights[0].size();
+        atlantic = vector<vector<int>>(heights.size(), vector<int>(heights[0].size(), 0));
+        pacific = vector<vector<int>>(heights.size(), vector<int>(heights[0].size(), 0));
+        for(int i = 0 ; i < length ; i++){
+            dfs(i, 0, heights, pacific);
+            dfs(i, width - 1, heights, atlantic);
+            // dfs(0, i, heights, pacific);
+            // dfs(length - 1, i, heights, atlantic);
+            // atlantic[i][0] += 1;
+            // atlantic[i][width-1] += 2;
+        }
+        for(int i = 0 ; i < width ; i++){
+            dfs(0, i, heights, pacific);
+            dfs(length - 1, i, heights, atlantic);
+        }
+        vector<vector<int>> result;
+        for(int i = 0 ; i < heights.size() ; i++){
+            for(int j = 0 ; j < heights[i].size() ; j++){
+                // int flag = dfs(i, j, heights);
+                // cout << atlantic[i][j] << ' ';
+                // if(flag >= 3) result.push_back(vector<int>{i, j});
+                if(pacific[i][j] && atlantic[i][j]) result.push_back(vector<int>{i, j});
+            }
+            // cout << endl;
+        }
+        return result;
+    }
+};
