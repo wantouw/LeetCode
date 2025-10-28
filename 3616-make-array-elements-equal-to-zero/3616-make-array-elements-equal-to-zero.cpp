ca41744101c98ce6ed40e08212432d557a4da053
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-10-28 15:35:55 +0700

    Time: 6 ms (49.08%), Space: 22.8 MB (43.12%) - LeetHub

---
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        vector<int> sums = vector<int>(nums.size(), 0);
+        // cout << "hai" << endl;
+        for(int i = 0 ; i < nums.size() ; i++){
+            // cout << i << endl;
+            
+            sums[i]+=nums[i];
+            if(i > 0) sums[i]+=sums[i-1];
+            // cout << i << endl;
+        }
+        // cout << "hai" << endl;
+        int result = 0;
+        int left = 0, right = 0;
+        for(int i = 0 ; i < nums.size() ;  i++){
+            if(nums[i]!=0) continue;
+            if(i > 0) left = sums[i-1];
+            right = sums[nums.size() - 1] - sums[i];
+            if(left == right) result += 2;
+            else if(abs(left - right) == 1) result++;
+        }
+        return result;
+    }
+};
