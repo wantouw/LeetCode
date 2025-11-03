ad3323d4d5625fd1ba72ade9a44bd38d4c036717
Ryan Wantouw <91063309+wantouw@users.noreply.github.com>
2025-11-03 16:19:01 +0700

    Time: 0 ms (100%), Space: 99.9 MB (35.11%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
@@ -4,23 +4,10 @@ public:
     {
         char curr = colors[0];
         int minimum = -1;
-        // int tracker = 0;
         int result = 0;
         int count = 0;
         for (int i = 0; i < colors.length(); i++)
         {
-            // if(colors[i] == curr){
-            // 	count++;
-            // 	minimum = min(minimum, neededTime[i]);
-            // 	result+=neededTime[i];
-            // }
-            // else {
-            // 	if(count > 1) result-=minimum;
-            // 	minimum = min(minimum, neededTime[i]);
-            // 	curr = colors[i];
-            // 	count++;
-            // 	result+=neededTime[i];
-            // }
             if (colors[i] != curr)
             {
                 result -= minimum;
@@ -31,7 +18,6 @@ public:
             minimum = max(minimum, neededTime[i]);
             count++;
             result += neededTime[i];
-            // cout << result << endl;
         }
 
         return result - minimum;
